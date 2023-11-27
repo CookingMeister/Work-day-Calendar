5f2bd5e6e0190093453a2b68485676e1f226a936
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2023-11-27 16:22:47 -0400

    text and hour are saved as an object in local storage

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-let time = JSON.parse(localStorage.getItem("timeLord")) || [];
-let hour;
-let text;
 $(window).on("DOMContentLoaded", () => {
   console.log("DOM loaded");
   function updateDateTime() {
@@ -11,57 +5,43 @@ $(window).on("DOMContentLoaded", () => {
     $("#currentDay").text(today.format("dddd | MMM. D, YYYY | h:mm A"));
   }
   setInterval(updateDateTime, 1000);
-
-  // $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // $(".saveBtn").on("click", function (event) {
-  //   event.preventDefault();
-  //   hour = this.parentNode.id;
-  //   console.log(hour);
-  //   // text = $("textarea").val();
-  //   text = $(".saveBtn").prev().val();
-  //   console.log(text);
-  // });
+  let eData = JSON.parse(localStorage.getItem("calendarData")) || {};
+  let hour;
+  let eventText;
+  let retrievedData;
+  let eventForHour;
 
   $(".saveBtn").each(function () {
     if ($(this).prev().val()) {
-      text = $(this).prev().val();
+      eventText = $(this).prev().val();
       $(".saveBtn").on("click", function (event) {
-        event.preventDefault();
+        // event.stopPropagation();
         hour = this.parentNode.id;
         console.log(hour);
-        text = [];
-        $(".saveBtn").each(function () {
-          text.push($(this).prev().val());
-        });
-        console.log(text);
-        addTime(hour, text);
+        eventText = $(this).prev().val();
+        console.log(eventText);
+        eData = JSON.parse(localStorage.getItem("calendarData"));
+        eData[hour] = eventText;
+        localStorage.setItem("calendarData", JSON.stringify(eData));
+        retrievedData = JSON.parse(localStorage.getItem("calendarData"));
+        eventForHour = retrievedData[hour];
+        console.log("Event for " + hour + ": " + eventForHour);
+       
       });
-
       return false;
     }
   });
-  function addTime(hour, text) {
-    time.push({ hour, text });
-    // Update local storage
-    localStorage.setItem("timeLord", JSON.stringify(time));
-    
-  }
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.<script>
+  retrievedData = JSON.parse(localStorage.getItem("calendarData"));
+  eventForHour = retrievedData[hour];
+  $(".saveBtn").prev().val(eventForHour);
 });
+
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
